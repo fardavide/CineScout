e2ac5efa4bd561072c467f4cc89a2c4a3761194e
Davide Farella <mail@4face.studio>
2020-08-27 18:35:20 +0200

    Handle back-stack

--- client/android/src/main/kotlin/client/android/MainActivity.kt
+++ client/android/src/main/kotlin/client/android/MainActivity.kt
@@ -4,10 +4,17 @@ package studio.forface.cinescout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.setContent
+import androidx.lifecycle.lifecycleScope
+import client.ViewState
+import client.android.AndroidNavigator
 import client.android.CineScoutApp
+import kotlinx.coroutines.flow.collect
 import org.koin.android.ext.android.getKoin
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private val navigator by inject<AndroidNavigator>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +22,18 @@ class MainActivity : AppCompatActivity() {
         setContent {
             CineScoutApp(getKoin())
         }
+
+        lifecycleScope.launchWhenCreated {
+            navigator.screen.collect {
+                if (it == ViewState.None) {
+                    navigator.toHome()
+                    super.onBackPressed()
+                }
+            }
+        }
     }
 
-    // TODO
-    //  override fun onBackPressed() {
-    //      if (!navigationViewModel.onBack()) {
-    //          super.onBackPressed()
-    //      }
-    //  }
+    override fun onBackPressed() {
+        navigator.back()
+    }
 }
