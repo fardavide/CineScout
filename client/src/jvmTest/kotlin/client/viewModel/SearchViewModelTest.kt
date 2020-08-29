ec445b7808fcd91c009e31cdf99e0e68521140de
Davide Farella <mail@4face.studio>
2020-08-29 12:41:30 +0200

    Fix SearchViewModelTest.kt
    Closes #18

--- client/src/jvmTest/kotlin/client/viewModel/SearchViewModelTest.kt
+++ client/src/jvmTest/kotlin/client/viewModel/SearchViewModelTest.kt
@@ -25,105 +25,109 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
 import kotlin.test.*
 
-// TODO THE FREAKING TEST WITH CHANNEL AND DISPATCHERS ARE FILING WITH: This job has not completed yet!!!!!!!!!!!!!
-//internal class SearchViewModelTest : ViewStateTest() {
-//
-//    private fun CoroutineScope.ViewModel(
-//        searchMovies: SearchMovies = SearchMovies(MockMovieRepository()),
-//    ) = SearchViewModel(
-//        this,
-//        TestDispatchersProvider(),
-//        searchMovies,
-//    )
-//
-//    @Test
-//    fun `Can catch exceptions and deliver with result`() = runBlockingTest {
-//        val vm = ViewModel(searchMovies = mockk {
-//            coEvery { this@mockk(any()) } answers {
-//                throw Exception("Something has happened")
-//            }
-//        })
-//
-//        vm search "no"
-//        assert that vm.result.next() `is` type<Error>()
-//
-//        vm.closeChannels()
-//    }
-//
-//    @Test
-//    fun `Returns right movies collection`() = runBlockingTest {
-//        val vm = ViewModel()
-//
-//        vm search "The Book"
-//        assert that vm.result.nextData().first() equals TheBookOfEli
-//
-//        vm search "The"
-//        assert that vm.result.nextData() `contains all` setOf(TheBookOfEli, TheGreatDebaters)
-//
-//        vm.closeChannels()
-//    }
-//
-//    @Test
-//    fun `Debounces too quick inputs`() = runBlockingTest {
-//        val spySearchMovies = spyk(SearchMovies(MockMovieRepository()))
-//        val vm = ViewModel(searchMovies = spySearchMovies)
-//
-//        val result = mutableListOf<Collection<Movie>>()
-//        val job2 = launch {
-//            vm.result.onlyData().toList(result)
-//        }
-//
-//        vm search "Fury"
-//        // No wait time, data is not available yes
-//        advanceTimeBy(0)
-//        assert that vm.result.data `is` Null
-//
-//        // Wait 500ms, data has been published
-//        advanceTimeBy(600)
-//        assert that vm.result.data!! `equals no order` setOf(Fury)
-//
-//        vm search "Inception"
-//        // Wait 500ms, data has been published
-//        advanceTimeBy(600)
-//        assert that vm.result.data!! `equals no order` setOf(Inception)
-//
-//        vm search "Pulp Fiction"
-//        // Short wait time, still old data
-//        advanceTimeBy(90)
-//        assert that vm.result.data!! `equals no order` setOf(Inception)
-//
-//        vm search "Sin City"
-//        // Wait 500ms, data has been published
-//        advanceTimeBy(600)
-//        assert that vm.result.data!! `equals no order` setOf(SinCity)
-//
-//        coVerify(exactly = 3) { spySearchMovies(any()) }
-//        assert that result.size equals 3
-//
-//        job2.cancel()
-//        vm.closeChannels()
-//    }
-//
-//    @Test
-//    fun `Does not search below 2 chars`() = runBlockingTest {
-//        val vm = ViewModel()
-//
-//        vm search "fury"
-//        assert that vm.result.next() `is` type<Success<Collection<Movie>>>()
-//
-//        vm search ""
-//        assert that vm.result.next() equals None
-//
-//        vm search "th"
-//        assert that vm.result.next() `is` type<Success<Collection<Movie>>>()
-//
-//        vm search "a"
-//        assert that vm.result.next() equals None
-//
-//        vm.closeChannels()
-//    }
-//}
+internal class SearchViewModelTest : ViewStateTest() {
+
+    private val dispatchers = TestDispatchersProvider()
+    private fun CoroutineScope.ViewModel(
+        searchMovies: SearchMovies = SearchMovies(MockMovieRepository()),
+    ): SearchViewModel {
+        return SearchViewModel(
+            this,
+            dispatchers,
+            searchMovies,
+        )
+    }
+
+    @Test
+    fun `Can catch exceptions and deliver with result`() = dispatchers.Main.runBlockingTest {
+        val vm = ViewModel(searchMovies = mockk {
+            coEvery { this@mockk(any()) } answers {
+                throw Exception("Something has happened")
+            }
+        })
+
+        vm search "no"
+        assert that vm.result.next() `is` type<Error>()
+
+        vm.closeChannels()
+        cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `Returns right movies collection`() = dispatchers.Main.runBlockingTest {
+        val vm = ViewModel()
+
+        vm search "The Book"
+        assert that vm.result.nextData().first() equals TheBookOfEli
+
+        vm search "The"
+        assert that vm.result.nextData() `contains all` setOf(TheBookOfEli, TheGreatDebaters)
+
+        vm.closeChannels()
+    }
+
+    @Test
+    fun `Debounces too quick inputs`() = dispatchers.Main.runBlockingTest {
+        val spySearchMovies = spyk(SearchMovies(MockMovieRepository()))
+        val vm = ViewModel(searchMovies = spySearchMovies)
+
+        val result = mutableListOf<Collection<Movie>>()
+        val job2 = launch {
+            vm.result.onlyData().toList(result)
+        }
+
+        vm search "Fury"
+        // No wait time, data is not available yes
+        advanceTimeBy(0)
+        assert that vm.result.data `is` Null
+
+        // Wait 500ms, data has been published
+        advanceTimeBy(600)
+        assert that vm.result.data!! `equals no order` setOf(Fury)
+
+        vm search "Inception"
+        // Wait 500ms, data has been published
+        advanceTimeBy(600)
+        assert that vm.result.data!! `equals no order` setOf(Inception)
+
+        vm search "Pulp Fiction"
+        // Short wait time, still old data
+        advanceTimeBy(90)
+        assert that vm.result.data!! `equals no order` setOf(Inception)
+
+        vm search "Sin City"
+        // Wait 500ms, data has been published
+        advanceTimeBy(600)
+        assert that vm.result.data!! `equals no order` setOf(SinCity)
+
+        coVerify(exactly = 3) { spySearchMovies(any()) }
+        assert that result.size equals 3
+
+        job2.cancel()
+        vm.closeChannels()
+    }
+
+    @Test
+    fun `Does not search below 2 chars`() = dispatchers.Main.runBlockingTest {
+        val vm = ViewModel()
+
+        vm search "fury"
+        assert that vm.result.next() `is` type<Success<Collection<Movie>>>()
+
+        vm search ""
+        assert that vm.result.next() equals None
+
+        vm search "th"
+        assert that vm.result.next() `is` type<Success<Collection<Movie>>>()
+
+        vm search "a"
+        assert that vm.result.next() equals None
+
+        vm.closeChannels()
+    }
+}
--- client/src/jvmTest/kotlin/entities/util/TestDispatchersProvider.kt
+++ client/src/jvmTest/kotlin/entities/util/TestDispatchersProvider.kt
@@ -5,6 +5,6 @@ import kotlinx.coroutines.test.TestCoroutineDispatcher
 
 class TestDispatchersProvider : DispatchersProvider {
     override val Main = TestCoroutineDispatcher()
-    override val Comp = TestCoroutineDispatcher()
-    override val Io = TestCoroutineDispatcher()
+    override val Comp = Main
+    override val Io = Main
 }
