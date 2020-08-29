24c353a1481dd37d22f7dc29c7b2895b1ff00941
Davide Farella <mail@4face.studio>
2020-08-29 17:40:41 +0200

    DiTest.kt for Android

---
+++ client/android/src/test/kotlin/client/android/DiTest.kt
@@ -0,0 +1,26 @@
+package client.android
+
+import client.viewModel.GetSuggestedMovieViewModel
+import client.viewModel.RateMovieViewModel
+import client.viewModel.SearchViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import org.junit.experimental.categories.Category
+import org.koin.core.parameter.parametersOf
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.category.CheckModuleTest
+import org.koin.test.check.checkModules
+import kotlin.test.*
+
+@Category(CheckModuleTest::class)
+class DiTest : AutoCloseKoinTest() {
+
+    @Test
+    fun checkModules() = checkModules(parameters = {
+        create<GetSuggestedMovieViewModel> { parametersOf(CoroutineScope(Job())) }
+        create<RateMovieViewModel> { parametersOf(CoroutineScope(Job())) }
+        create<SearchViewModel> { parametersOf(CoroutineScope(Job())) }
+    }) {
+        modules(androidClientModule)
+    }
+}
