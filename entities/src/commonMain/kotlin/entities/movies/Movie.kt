5980e5277b24e459994ded8fe888485e43c8bbb9
Davide Farella <mail@4face.studio>
2020-08-30 10:50:15 +0200

    Add Poster to Movie.kt and doubles.kt

--- entities/src/commonMain/kotlin/entities/movies/Movie.kt
+++ entities/src/commonMain/kotlin/entities/movies/Movie.kt
@@ -3,12 +3,15 @@ package entities.movies
 import entities.Actor
 import entities.Genre
 import entities.Name
+import entities.Poster
 import entities.TmdbId
 
 data class Movie(
     val id: TmdbId,
     val name: Name,
+    private val poster: Poster?,
     val actors: Collection<Actor>,
     val genres: Collection<Genre>,
     val year: UInt
-)
+) {
+}
