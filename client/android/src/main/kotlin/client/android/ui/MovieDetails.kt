d02d3a043c0a5216c1a4dda11be2bcb1462f847d
Davide Farella <mail@4face.studio>
2020-08-30 10:17:33 +0200

    Improve TextStyle in MovieDetails.kt

--- client/android/src/main/kotlin/client/android/ui/MovieDetails.kt
+++ client/android/src/main/kotlin/client/android/ui/MovieDetails.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import client.android.widget.CenteredText
 import client.resource.Strings
 import entities.movies.Movie
@@ -17,13 +20,15 @@ import entities.movies.Movie
 fun MovieDetails(movie: Movie) {
 
     Column(
-        Modifier.fillMaxSize(),
+        Modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalGravity = Alignment.CenterHorizontally
     ) {
 
         MovieTitle(title = movie.name.s)
-        MovieBody(genres = movie.genres.joinToString { it.name.s }, actors = movie.actors.joinToString { it.name.s })
+        MovieBody(
+            genres = movie.genres.joinToString { it.name.s },
+            actors = movie.actors.take(5).joinToString { it.name.s })
     }
 }
 
@@ -34,19 +39,25 @@ fun MovieDetails(movie: Movie) {
 
 @Composable private fun MovieTitle(title: String) {
 
-    CenteredText(style = MaterialTheme.typography.h5, text = title)
+    CenteredText(style = MaterialTheme.typography.h4, text = title)
 }
 
 @Composable
 private fun MovieBody(genres: String, actors: String) {
 
     Row {
-        Text(text = Strings.GenresTitle)
-        Text(style = MaterialTheme.typography.body2, text = genres)
+        Column {
+
+            Text(text = Strings.GenresTitle)
+            Text(style = MaterialTheme.typography.h6, text = genres)
+        }
     }
 
     Row {
-        Text(text = Strings.ActorsTitle)
-        Text(style = MaterialTheme.typography.body2, text = actors)
+        Column {
+
+            Text(text = Strings.ActorsTitle)
+            Text(style = MaterialTheme.typography.h6, textAlign = TextAlign.Center, text = actors)
+        }
     }
 }
