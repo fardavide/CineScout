3069d99f24c1775aed869b7bdf10ab0a254ba751
Davide Farella <mail@4face.studio>
2020-09-06 16:37:22 +0200

    GetSuggestedMovieViewModelTest.kt to use viewModelTest

--- client/src/jvmTest/kotlin/client/viewModel/GetSuggestedMovieViewModelTest.kt
+++ client/src/jvmTest/kotlin/client/viewModel/GetSuggestedMovieViewModelTest.kt
@@ -44,16 +44,17 @@ internal class GetSuggestedMovieViewModelTest : ViewModelTest {
     }
 
     @Test
-    fun `Can catch exceptions and deliver with result`() = coroutinesTest {
-        val vm = ViewModel(getSuggestedMovies = mockk {
+    fun `Can catch exceptions and deliver with result`() = viewModelTest({
+
+        ViewModel(getSuggestedMovies = mockk {
             coEvery { this@mockk(any<Int>()) } answers {
                 throw Exception("Something has happened")
             }
         })
 
-        assert that vm.result.state `is` type<Error>()
+    }) { viewModel ->
 
-        vm.closeChannels()
+        assert that viewModel.result.state `is` type<Error>()
     }
 
     @Test
@@ -72,101 +73,101 @@ internal class GetSuggestedMovieViewModelTest : ViewModelTest {
     }
 
     @Test
-    fun `Skip shown next movie`() = coroutinesTest {
+    fun `Skip shown next movie`() = viewModelTest({
 
         // Add some like movies for give more data to the test
         rateMovie(DejaVu, Rating.Positive)
         rateMovie(Inception, Rating.Positive)
         rateMovie(TheGreatDebaters, Rating.Positive)
 
-        val vm = ViewModel(randomize = false)
+        ViewModel(randomize = false)
 
-        val first = vm.result.state
+    }) { viewModel ->
+
+        val first = viewModel.result.state
         assert that first `is` type<Success<Movie>>()
 
-        vm.skipCurrent()
-        val second = vm.result.data
+        viewModel.skipCurrent()
+        val second = viewModel.result.data
         assert that second *{
             it `is not` Null
             it `not equals` first.data
         }
-
-        vm.closeChannels()
     }
 
     @Test
-    fun `Does not stop delivering suggestions`() = coroutinesTest {
+    fun `Does not stop delivering suggestions`() = viewModelTest({
 
         // Add some like movies for give more data to the test
         rateMovie(Inception, Rating.Positive)
         rateMovie(TheBookOfEli, Rating.Positive)
         rateMovie(TheGreatDebaters, Rating.Positive)
 
-        val vm = ViewModel(randomize = false)
+        ViewModel(randomize = false)
+
+    }) { viewModel ->
 
         var last: MovieCount? = null
 
         repeat(100) { count ->
-            val current = vm.result.data
+            val current = viewModel.result.data
             assert that current * {
                 it `is not` Null { "Error on iteration #$count" }
                 +(this + count) `not equals` last
             }
             last = current + count
-            vm.skipCurrent()
+            viewModel.skipCurrent()
         }
-
-        vm.closeChannels()
     }
 
     @Test
-    fun `Does not show liked movies`() = coroutinesTest {
+    fun `Does not show liked movies`() = viewModelTest({
 
         // Add some like movies for give more data to the test
         rateMovie(TheBookOfEli, Rating.Positive)
         rateMovie(TheGreatDebaters, Rating.Positive)
 
-        val vm = ViewModel(randomize = false)
+        ViewModel(randomize = false)
 
-        val current = vm.result.data
+    }) { viewModel ->
+
+        val current = viewModel.result.data
         assert that current `is` type<Movie>()
 
-        vm.likeCurrent()
+        viewModel.likeCurrent()
 
         repeat(100) { count ->
-            assert that vm.result.data * {
+            assert that viewModel.result.data * {
                 it `is not` Null { "Error on iteration #$count" }
                 it `not equals` current { "Error on iteration #$count" }
             }
-            vm.skipCurrent()
+            viewModel.skipCurrent()
         }
-
-        vm.closeChannels()
     }
 
     @Test
-    fun `Does not show disliked movies`() = coroutinesTest {
+    fun `Does not show disliked movies`() = viewModelTest({
 
         // Add some like movies for give more data to the test
         rateMovie(TheBookOfEli, Rating.Positive)
         rateMovie(TheGreatDebaters, Rating.Positive)
 
-        val vm = ViewModel(randomize = false)
+        ViewModel(randomize = false)
 
-        val current = vm.result.data
+    }) { viewModel ->
+
+        val current = viewModel.result.data
         assert that current `is` type<Movie>()
 
-        vm.dislikeCurrent()
+        viewModel.dislikeCurrent()
 
         repeat(100) { count ->
-            assert that vm.result.data * {
+            assert that viewModel.result.data * {
                 it `is not` Null { "Error on iteration #$count" }
                 it `not equals` current { "Error on iteration #$count" }
             }
-            vm.skipCurrent()
+            viewModel.skipCurrent()
         }
-
-        vm.closeChannels()
     }
 
     data class MovieCount(val movie: Movie, val count: Int,)
