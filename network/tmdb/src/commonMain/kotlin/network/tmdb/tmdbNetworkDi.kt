ba96ddcb2c7f525a7c4a2b148ef73fb54a37b088
Davide Farella <mail@4face.studio>
2020-11-04 18:27:26 +0100

    Improve consistency across Tmdb clients

--- network/tmdb/src/commonMain/kotlin/network/tmdb/tmdbNetworkDi.kt
+++ network/tmdb/src/commonMain/kotlin/network/tmdb/tmdbNetworkDi.kt
@@ -2,6 +2,7 @@ package network.tmdb
 
 import io.ktor.client.HttpClient
 import io.ktor.client.features.defaultRequest
+import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.headers
 import io.ktor.client.request.parameter
 import io.ktor.http.URLProtocol
@@ -9,6 +10,7 @@ import network.baseHttpClient
 import network.networkModule
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import util.takeIfNotBlank
 
 val v3Client = named("Tmdb client v3")
 val v4Client = named("Tmdb client v4")
@@ -16,9 +18,15 @@ val v4accessToken = named("Tmdb v4 access token")
 
 val tmdbNetworkModule = module {
 
+    fun HttpRequestBuilder.headers(accessToken: String) = headers {
+        append("Content-Type", "application/json;charset=utf-8")
+        append("Authorization", "Bearer ${accessToken.takeIfNotBlank() ?: TMDB_V4_READ_ACCESS_TOKEN}")
+    }
+
     single(v3Client) {
         get<HttpClient>(baseHttpClient).config {
             defaultRequest {
+                headers(get(v4accessToken))
                 url {
                     protocol = URLProtocol.HTTPS
                     host = "api.themoviedb.org/3"
@@ -31,14 +39,12 @@ val tmdbNetworkModule = module {
     single(v4Client) {
         get<HttpClient>(baseHttpClient).config {
             defaultRequest {
+                headers(get(v4accessToken))
                 url {
                     protocol = URLProtocol.HTTPS
                     host = "api.themoviedb.org/4"
                 }
-                headers {
-                    append("Content-Type", "application/json;charset=utf-8")
-                    append("Authorization", "Bearer ${get<String>(v4accessToken)}")
-                }
+                parameter("api_key", TMDB_V3_API_KEY)
             }
         }
     }
