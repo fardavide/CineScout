cd67469b6b24beb33990e5886c3fbb61e03625b6
Davide Farella <mail@4face.studio>
2022-07-03 13:41:22 +0200

    Update .gitignore

--- cinescout/di/src/commonMain/kotlin/cinescout/di/CineScoutModule.kt
+++ cinescout/di/src/commonMain/kotlin/cinescout/di/CineScoutModule.kt
@@ -3,12 +3,13 @@ package cinescout.di
 import cinescout.movies.data.MoviesDataModule
 import cinescout.movies.domain.MoviesDomainModule
 import cinescout.network.NetworkModule
+import cinescout.network.tmdb.TmdbNetworkModule
 import cinescout.network.trakt.TraktNetworkModule
 import cinescout.suggestions.domain.SuggestionsDomainModule
 import org.koin.dsl.module
 
 val CineScoutModule = module {
     includes(MoviesDataModule, MoviesDomainModule)
-    includes(NetworkModule, TraktNetworkModule, TraktNetworkModule)
+    includes(NetworkModule, TmdbNetworkModule, TraktNetworkModule)
     includes(SuggestionsDomainModule)
 }
---
+++ cinescout/network/tmdb/src/commonMain/kotlin/cinescout/network/tmdb/tmdbKeys.kt
@@ -0,0 +1,4 @@
+package cinescout.network.tmdb
+
+internal const val TMDB_V3_API_KEY = "Your Tmdb V3 api key"
+internal const val TMDB_V4_READ_ACCESS_TOKEN = "Your Tmdb V4 read access token"
