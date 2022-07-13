fa6caeda506f93083d765c25c0905dfc763f1067
Davide Farella <fardavide@pm.me>
2022-07-13 12:01:04 +0200

    Create PagedStore

--- cinescout/utils/kotlin/src/commonMain/kotlin/cinescout/utils/kotlin/Store.kt
+++ cinescout/utils/kotlin/src/commonMain/kotlin/cinescout/utils/kotlin/Store.kt
@@ -5,7 +5,11 @@ import arrow.core.right
 import cinescout.error.DataError
 import cinescout.error.NetworkError
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combineTransform
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.transform
 
 /**
  * Creates a flow that combines Local data and Remote data
@@ -22,7 +26,50 @@ fun <T> Store(
     write: suspend (T) -> Unit
 ): Store<T> = StoreImpl(buildStoreFlow(fetch, read, write))
 
+fun <T, B> PagedStore(
+    initialBookmark: B,
+    createNextBookmark: (lastData: T, currentBookmark: B) -> B,
+    fetch: suspend (bookmark: B) -> Either<NetworkError, T>,
+    read: () -> Flow<Either<DataError.Local, T>>,
+    write: suspend (T) -> Unit
+): PagedStore<T> {
+    var bookmark = initialBookmark
+    var lastData: T? = null
+    val loadMoreTrigger = MutableStateFlow(initialBookmark)
+    val onLoadMore = { loadMoreTrigger.value = bookmark }
+    var shouldLoadAll = false
+
+    val flow = buildPagedStoreFlow(
+        fetch = { fetch(bookmark).tap { data -> bookmark = createNextBookmark(data, bookmark) } },
+        read = read,
+        write = write,
+        loadMoreTrigger = loadMoreTrigger
+    ).onEach { either ->
+        either.tap { data ->
+            if (shouldLoadAll && lastData != data) {
+                onLoadMore()
+            }
+            lastData = data
+        }
+    }
+    return PagedStoreImpl(
+        flow = flow,
+        onLoadMore = onLoadMore,
+        onLoadAll = {
+            shouldLoadAll = true
+            onLoadMore()
+        }
+    )
+}
+
 interface Store<T> : Flow<Either<DataError.Remote<T>, T>>
+
+interface PagedStore<T> : Store<T> {
+
+    fun loadAll()
+
+    fun loadMore()
+}
 
 private fun <T> buildStoreFlow(
     fetch: suspend () -> Either<NetworkError, T>,
@@ -30,14 +77,39 @@ private fun <T> buildStoreFlow(
     write: suspend (T) -> Unit
 ): Flow<Either<DataError.Remote<T>, T>> =
     combineTransform(
-        ticker(DataRefreshInterval) {
-            emit(null)
+        ticker<Either<NetworkError, T>?>(DataRefreshInterval) {
             val remoteDataEither = fetch()
             remoteDataEither.tap { remoteData ->
                 write(remoteData)
             }
             emit(remoteDataEither)
-        },
+        }.onStart { emit(null) },
+        read()
+    ) { remoteEither, localEither ->
+        if (remoteEither != null) {
+            val remote = remoteEither.mapLeft { networkError ->
+                DataError.Remote(localData = localEither, networkError = networkError)
+            }
+            emit(remote)
+        } else {
+            localEither.tap { local -> emit(local.right()) }
+        }
+    }
+
+private fun <T, B> buildPagedStoreFlow(
+    fetch: suspend (bookmark: B) -> Either<NetworkError, T>,
+    read: () -> Flow<Either<DataError.Local, T>>,
+    write: suspend (T) -> Unit,
+    loadMoreTrigger: Flow<B>
+): Flow<Either<DataError.Remote<T>, T>> =
+    combineTransform(
+        loadMoreTrigger.transform<B, Either<NetworkError, T>?> { bookmark ->
+            val remoteDataEither = fetch(bookmark)
+            remoteDataEither.tap { remoteData ->
+                write(remoteData)
+            }
+            emit(remoteDataEither)
+        }.onStart { emit(null) },
         read()
     ) { remoteEither, localEither ->
         if (remoteEither != null) {
@@ -52,3 +124,19 @@ private fun <T> buildStoreFlow(
 
 private class StoreImpl<T>(flow: Flow<Either<DataError.Remote<T>, T>>) :
     Store<T>, Flow<Either<DataError.Remote<T>, T>> by flow
+
+private class PagedStoreImpl<T>(
+    flow: Flow<Either<DataError.Remote<T>, T>>,
+    private val onLoadMore: () -> Unit,
+    private val onLoadAll: () -> Unit
+) :
+    PagedStore<T>, Flow<Either<DataError.Remote<T>, T>> by flow {
+
+    override fun loadAll() {
+        onLoadAll()
+    }
+
+    override fun loadMore() {
+        onLoadMore()
+    }
+}
--- cinescout/utils/kotlin/src/commonTest/kotlin/cinescout/utils/kotlin/StoreTest.kt
+++ cinescout/utils/kotlin/src/commonTest/kotlin/cinescout/utils/kotlin/StoreTest.kt
@@ -85,6 +85,68 @@ internal class StoreTest {
         }
     }
 
+    @Test
+    fun `paged store loads more`() = runTest {
+        // given
+        val localData = 1.right()
+        fun loadRemoteData(bookmark: Int) = bookmark.right()
+
+        val localFlow = MutableStateFlow(localData)
+
+        val store = PagedStore(
+            initialBookmark = 2,
+            createNextBookmark = { _, currentBookmark -> currentBookmark + 1 },
+            fetch = { bookmark ->
+                delay(NetworkDelay)
+                loadRemoteData(bookmark)
+            },
+            write = { localFlow.emit(it.right()) },
+            read = { localFlow }
+        )
+
+        // when
+        store.test {
+
+            // then
+            assertEquals(localData, awaitItem())
+            assertEquals(2.right(), awaitItem())
+            store.loadMore()
+            assertEquals(3.right(), awaitItem())
+        }
+    }
+
+    @Test
+    fun `paged store loads all`() = runTest {
+        // given
+        val localData = 1.right()
+        fun loadRemoteData(bookmark: Int) = bookmark.coerceAtMost(5).right()
+
+        val localFlow = MutableStateFlow(localData)
+
+        val store = PagedStore(
+            initialBookmark = 2,
+            createNextBookmark = { _, currentBookmark -> currentBookmark + 1 },
+            fetch = { bookmark ->
+                delay(NetworkDelay)
+                loadRemoteData(bookmark)
+            },
+            write = { localFlow.emit(it.right()) },
+            read = { localFlow }
+        )
+
+        // when
+        store.test {
+
+            // then
+            assertEquals(localData, awaitItem())
+            assertEquals(2.right(), awaitItem())
+            store.loadAll()
+            assertEquals(3.right(), awaitItem())
+            assertEquals(4.right(), awaitItem())
+            assertEquals(5.right(), awaitItem())
+        }
+    }
+
     companion object {
 
         private const val NetworkDelay = 100L
