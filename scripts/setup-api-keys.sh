059656397f9d2ff8c3143166558d61b641025036
Davide Farella <fardavide@pm.me>
2022-07-17 09:52:04 +0200

    Revert script for create API keys

--- scripts/setup-api-keys.sh
+++
@@ -1,2 +0,0 @@
-echo "$TMDB_KEYS" > "$GITHUB_WORKSPACE"/cinescout/network/tmdb/src/commonMain/kotlin/cinescout/network/tmdb/tmdbKeys.kt &&
-  echo "$TRAKT_KEYS" > "$GITHUB_WORKSPACE"/cinescout/network/trakt/src/commonMain/kotlin/cinescout/network/trakt/traktKeys.kt
