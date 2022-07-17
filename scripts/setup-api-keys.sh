86eaa2efbe0e860478accd6b69f7fd6264bf1ed3
Davide Farella <fardavide@pm.me>
2022-07-17 09:23:27 +0200

    Bash script for setup API keys

---
+++ scripts/setup-api-keys.sh
@@ -0,0 +1,2 @@
+echo "$TMDB_KEYS" > "$GITHUB_WORKSPACE"/cinescout/network/tmdb/src/commonMain/kotlin/cinescout/network/tmdb/tmdbKeys.kt &&
+  echo "$TRAKT_KEYS" > "$GITHUB_WORKSPACE"/cinescout/network/trakt/src/commonMain/kotlin/cinescout/network/trakt/traktKeys.kt
