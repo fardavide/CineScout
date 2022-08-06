5d14420ebc82dd63ff85ae6c3c41f83158867c81
Davide Farella <fardavide@pm.me>
2022-08-06 09:46:33 +0200

    Override MovieRating.equals to ignore similar values

--- cinescout/movies/domain/src/commonMain/kotlin/cinescout/movies/domain/model/MovieRating.kt
+++ cinescout/movies/domain/src/commonMain/kotlin/cinescout/movies/domain/model/MovieRating.kt
@@ -1,6 +1,15 @@
 package cinescout.movies.domain.model
 
+import kotlin.math.roundToInt
+
 data class MovieRating(
     val voteCount: Int,
     val average: Rating
-)
+) {
+
+    override fun equals(other: Any?) =
+        other is MovieRating && hashCode() == other.hashCode()
+
+    override fun hashCode() =
+        voteCount / 100 + (average.value * 10).roundToInt()
+}
