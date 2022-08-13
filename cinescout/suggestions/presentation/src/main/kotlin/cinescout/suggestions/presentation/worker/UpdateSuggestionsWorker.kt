ab8f0128819f79ddb170854e1afa88e0c7cdda0e
Davide Farella <fardavide@pm.me>
2022-08-13 15:56:55 +0200

    Schedule OneTime worker to update suggestions after login
    Closes #106

--- cinescout/suggestions/presentation/src/main/kotlin/cinescout/suggestions/presentation/worker/UpdateSuggestionsWorker.kt
+++ cinescout/suggestions/presentation/src/main/kotlin/cinescout/suggestions/presentation/worker/UpdateSuggestionsWorker.kt
@@ -5,7 +5,9 @@ import androidx.work.BackoffPolicy
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.ExistingWorkPolicy
 import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
@@ -18,6 +20,7 @@ import cinescout.utils.android.setInput
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 
 class UpdateSuggestionsWorker(
@@ -44,9 +47,38 @@ class UpdateSuggestionsWorker(
     class Scheduler(private val workManager: WorkManager) {
 
         operator fun invoke(suggestionsMode: SuggestionsMode) {
+            when (suggestionsMode) {
+                SuggestionsMode.Deep -> schedulePeriodic()
+                SuggestionsMode.Quick -> scheduleExpedited()
+            }
+        }
+
+        private fun scheduleExpedited() {
             val constraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED)
-                .apply { setRequiresCharging(suggestionsMode == SuggestionsMode.Deep) }
+                .build()
+
+            val requestBuilder = OneTimeWorkRequestBuilder<UpdateSuggestionsWorker>()
+
+            val request = requestBuilder
+                .setConstraints(constraints)
+                .setInput(SuggestionsMode.Quick)
+                .setBackoffCriteria(BackoffPolicy.LINEAR, Backoff.toJavaDuration())
+                // TODO needs notification
+                // .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                .build()
+
+            workManager.enqueueUniqueWork(
+                ExpeditedName,
+                ExistingWorkPolicy.REPLACE,
+                request
+            )
+        }
+
+        private fun schedulePeriodic() {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresCharging(true)
                 .build()
 
             val requestBuilder = PeriodicWorkRequestBuilder<UpdateSuggestionsWorker>(
@@ -56,12 +88,12 @@ class UpdateSuggestionsWorker(
 
             val request = requestBuilder
                 .setConstraints(constraints)
-                .setInput(suggestionsMode)
-                .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, FlexInterval.toJavaDuration())
+                .setInput(SuggestionsMode.Deep)
+                .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, Backoff.toJavaDuration())
                 .build()
 
             workManager.enqueueUniquePeriodicWork(
-                Name,
+                PeriodicName,
                 ExistingPeriodicWorkPolicy.REPLACE,
                 request
             )
@@ -70,7 +102,9 @@ class UpdateSuggestionsWorker(
 
     companion object {
 
-        const val Name = "UpdateSuggestionsWorker"
+        const val ExpeditedName = "UpdateSuggestionsWorker.expedited"
+        const val PeriodicName = "UpdateSuggestionsWorker.periodic"
+        val Backoff = 10.seconds
         val Interval = 6.hours
         val FlexInterval = 3.hours
     }
