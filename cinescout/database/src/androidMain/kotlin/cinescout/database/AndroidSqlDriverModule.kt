0db810763eba485b7852f9090e000c583e8290b1
Davide Farella <fardavide@pm.me>
2022-09-16 08:25:49 +0200

    Set database journal as WAL

--- cinescout/database/src/androidMain/kotlin/cinescout/database/AndroidSqlDriverModule.kt
+++ cinescout/database/src/androidMain/kotlin/cinescout/database/AndroidSqlDriverModule.kt
@@ -1,5 +1,6 @@
 package cinescout.database
 
+import androidx.sqlite.db.SupportSQLiteDatabase
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import org.koin.dsl.module
@@ -7,7 +8,25 @@ import org.koin.dsl.module
 actual val SqlDriverModule = module {
 
     single<SqlDriver> {
-        val driver = AndroidSqliteDriver(context = get(), schema = Database.Schema, name = "cinescout.db")
+        val driver = AndroidSqliteDriver(
+            context = get(),
+            schema = Database.Schema,
+            name = "cinescout.db",
+            cacheSize = 50,
+            callback = object : AndroidSqliteDriver.Callback(Database.Schema) {
+                override fun onConfigure(db: SupportSQLiteDatabase) {
+                    super.onConfigure(db)
+                    db.enableWriteAheadLogging()
+                    setPragma(db, "JOURNAL_MODE = WAL")
+                    setPragma(db, "SYNCHRONOUS = 2")
+                }
+
+                private fun setPragma(db: SupportSQLiteDatabase, pragma: String) {
+                    val cursor = db.query("PRAGMA $pragma")
+                    cursor.close()
+                }
+            }
+        )
         driver.also { Database.Schema.create(driver) }
     }
 }
