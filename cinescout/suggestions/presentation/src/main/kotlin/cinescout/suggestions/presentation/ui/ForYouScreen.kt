b8ab3d1656f5081bb0adf78915aabff51a3274ef
Davide Giuseppe Farella <fardavide@gmail.com>
2022-10-20 08:14:47 +0200

    Add ForYouScreen.Mode

--- cinescout/suggestions/presentation/src/main/kotlin/cinescout/suggestions/presentation/ui/ForYouScreen.kt
+++ cinescout/suggestions/presentation/src/main/kotlin/cinescout/suggestions/presentation/ui/ForYouScreen.kt
@@ -1,8 +1,8 @@
 package cinescout.suggestions.presentation.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,12 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.Dp
 import cinescout.design.TestTag
 import cinescout.design.theme.CineScoutTheme
 import cinescout.design.theme.Dimens
@@ -30,8 +32,13 @@ import cinescout.movies.domain.model.TmdbMovieId
 import cinescout.search.presentation.ui.SearchLikedMovieScreen
 import cinescout.suggestions.presentation.model.ForYouAction
 import cinescout.suggestions.presentation.model.ForYouState
+import cinescout.suggestions.presentation.model.ForYouType
 import cinescout.suggestions.presentation.previewdata.ForYouScreenPreviewDataProvider
 import cinescout.suggestions.presentation.viewmodel.ForYouViewModel
+import cinescout.utils.compose.Adaptive
+import cinescout.utils.compose.WindowHeightSizeClass
+import cinescout.utils.compose.WindowSizeClass
+import cinescout.utils.compose.WindowWidthSizeClass
 import co.touchlab.kermit.Logger
 import org.koin.androidx.compose.koinViewModel
 import studio.forface.cinescout.design.R.string
@@ -70,20 +77,46 @@ internal fun ForYouScreen(
         actions.toForYouHint()
     }
 
-    Box(
-        modifier = modifier
-            .testTag(TestTag.ForYou)
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        when (val suggestedMovie = state.suggestedMovie) {
-            is ForYouState.SuggestedMovie.Data -> ForYouMovieItem(
-                model = suggestedMovie.movie,
-                actions = itemActions
-            )
-            is ForYouState.SuggestedMovie.Error -> ErrorScreen(text = suggestedMovie.message)
-            ForYouState.SuggestedMovie.Loading -> CenteredProgress()
-            ForYouState.SuggestedMovie.NoSuggestions -> NoSuggestionsScreen(searchLikedMovieScreen)
+    Adaptive { windowSizeClass ->
+        val mode = ForYouScreen.Mode.forClass(windowSizeClass)
+        @Composable
+        fun Content() {
+            when (val suggestedMovie = state.suggestedMovie) {
+                is ForYouState.SuggestedMovie.Data -> ForYouMovieItem(
+                    model = suggestedMovie.movie,
+                    actions = itemActions
+                )
+                is ForYouState.SuggestedMovie.Error -> ErrorScreen(text = suggestedMovie.message)
+                ForYouState.SuggestedMovie.Loading -> CenteredProgress()
+                ForYouState.SuggestedMovie.NoSuggestions -> NoSuggestionsScreen(searchLikedMovieScreen)
+            }
+        }
+        when (mode) {
+            is ForYouScreen.Mode.Vertical -> Column(
+                modifier = modifier
+                    .testTag(TestTag.ForYou)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ForYouTypeSelector(
+                    type = ForYouType.Movies,
+                    onTypeSelected = {}
+                )
+                Content()
+            }
+            ForYouScreen.Mode.Horizontal -> Row(
+                modifier = modifier
+                    .testTag(TestTag.ForYou)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                ForYouTypeSelector(
+                    modifier = Modifier.rotate(270f),
+                    type = ForYouType.Movies,
+                    onTypeSelected = {}
+                )
+                Content()
+            }
         }
     }
 }
@@ -91,7 +124,9 @@ internal fun ForYouScreen(
 @Composable
 private fun NoSuggestionsScreen(searchLikedMovieScreen: @Composable () -> Unit) {
     Column(
-        modifier = Modifier.fillMaxSize().padding(Dimens.Margin.Small),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(Dimens.Margin.Small),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -117,6 +152,28 @@ object ForYouScreen {
         companion object {
 
             val Empty = Actions(login = {}, toMovieDetails = {}, toForYouHint = {})
+        }
+    }
+
+    sealed interface Mode {
+
+        object Horizontal : Mode
+        data class Vertical(val spacing: Dp) : Mode
+
+        companion object {
+
+            fun forClass(windowSizeClass: WindowSizeClass): Mode {
+                return when (windowSizeClass.width) {
+                    WindowWidthSizeClass.Compact -> Vertical(spacing = Dimens.Margin.Medium)
+                    WindowWidthSizeClass.Medium -> Vertical(spacing = Dimens.Margin.Large)
+                    WindowWidthSizeClass.Expanded -> when (windowSizeClass.height) {
+                        WindowHeightSizeClass.Compact,
+                        WindowHeightSizeClass.Medium -> Horizontal
+
+                        WindowHeightSizeClass.Expanded -> Vertical(spacing = Dimens.Margin.XLarge)
+                    }
+                }
+            }
         }
     }
 }
