8373934e7026f8752b272613813fa02e016cda9e
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-13 18:07:52 +0100

    chore(build): setup ModulesCatalogPlugin

--- plugins/modules-catalog/src/main/kotlin/CineScoutModulesCatalogPlugin.kt
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-
-abstract class CineScoutModulesCatalogPlugin : Plugin<Project> {
-
-    override fun apply(target: Project) {
-        target.subprojects.forEach(CineScoutModulesCatalogExtension::setup)
-    }
-}
