8c1b5ea23694fcd961b90b890c42a1d6ab04dbdb
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-13 19:39:23 +0100

    chore(build): setup Android plugins

---
+++ plugins/android/src/main/kotlin/cinescout.plugins/android/AndroidDefaults.kt
@@ -0,0 +1,10 @@
+package cinescout.plugins.android
+
+object AndroidDefaults {
+
+    const val APPLICATION_ID = "studio.forface.cinescout"
+    const val BUILD_TOOLS = "33.0.2"
+    const val COMPILE_SDK = 33
+    const val MIN_SDK = 26
+    const val TARGET_SDK = 33
+}
