359896acf7c078f469bc15a098230a0b001ce476
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-20 08:19:55 +0100

    test(auth): refactor IsTraktLinkedTest to use Kotest

--- cinescout/auth/trakt/domain/src/commonMain/kotlin/cinescout/auth/trakt/domain/TraktAuthRepository.kt
+++ cinescout/auth/trakt/domain/src/commonMain/kotlin/cinescout/auth/trakt/domain/TraktAuthRepository.kt
@@ -1,9 +1,12 @@
 package cinescout.auth.trakt.domain
 
 import arrow.core.Either
+import arrow.core.right
 import cinescout.auth.trakt.domain.model.TraktAuthorizationCode
 import cinescout.auth.trakt.domain.usecase.LinkToTrakt
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
 
 interface TraktAuthRepository {
 
@@ -12,4 +15,24 @@ interface TraktAuthRepository {
     fun link(): Flow<Either<LinkToTrakt.Error, LinkToTrakt.State>>
 
     suspend fun notifyAppAuthorized(code: TraktAuthorizationCode)
+}
+
+class FakeTraktAuthRepository(
+    isLinked: Boolean = false
+) : TraktAuthRepository {
+
+    var invokedNotifyAppAuthorizedCode: TraktAuthorizationCode? = null
+        private set
+    private val mutableIsLinked = MutableStateFlow(isLinked)
+
+    override fun isLinked(): Flow<Boolean> = mutableIsLinked
+
+    override fun link(): Flow<Either<LinkToTrakt.Error, LinkToTrakt.State>> {
+        mutableIsLinked.value = true
+        return flowOf(LinkToTrakt.State.Success.right())
+    }
+
+    override suspend fun notifyAppAuthorized(code: TraktAuthorizationCode) {
+        invokedNotifyAppAuthorizedCode = code
+    }
 }
--- cinescout/auth/trakt/domain/src/commonTest/kotlin/cinescout/auth/trakt/domain/usecase/IsTraktLinkedTest.kt
+++ cinescout/auth/trakt/domain/src/commonTest/kotlin/cinescout/auth/trakt/domain/usecase/IsTraktLinkedTest.kt
@@ -1,34 +1,37 @@
-// package cinescout.auth.trakt.domain.usecase
-//
-// import app.cash.turbine.test
-// import cinescout.auth.trakt.domain.TraktAuthRepository
-// import io.mockk.coEvery
-// import io.mockk.coVerify
-// import io.mockk.mockk
-// import kotlinx.coroutines.flow.flowOf
-// import kotlinx.coroutines.test.runTest
-// import kotlin.test.Test
-// import kotlin.test.assertEquals
-//
-// class IsTraktLinkedTest {
-//
-//    private val repository: TraktAuthRepository = mockk {
-//        coEvery { isLinked() } returns flowOf(true)
-//    }
-//    private val isLinked = IsTraktLinked(repository)
-//
-//    @Test
-//    fun `does call repository`() = runTest {
-//        // given
-//        val expected = true
-//
-//        // when
-//        isLinked().test {
-//
-//            // then
-//            assertEquals(expected, awaitItem())
-//            awaitComplete()
-//            coVerify { repository.isLinked() }
-//        }
-//    }
-// }
+package cinescout.auth.trakt.domain.usecase
+
+import app.cash.turbine.test
+import cinescout.auth.trakt.domain.FakeTraktAuthRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class IsTraktLinkedTest : BehaviorSpec({
+
+    Given("is linked in repository") {
+        val repository = FakeTraktAuthRepository(isLinked = true)
+        val isLinked = RealIsTraktLinked(repository)
+
+        When("use case is called") {
+            isLinked().test {
+
+                Then("it returns true") {
+                    awaitItem() shouldBe true
+                }
+            }
+        }
+    }
+
+    Given("is not linked in repository") {
+        val repository = FakeTraktAuthRepository(isLinked = false)
+        val isLinked = RealIsTraktLinked(repository)
+
+        When("use case is called") {
+            isLinked().test {
+
+                Then("it returns false") {
+                    awaitItem() shouldBe false
+                }
+            }
+        }
+    }
+})
