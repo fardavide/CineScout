f6468d7e712ac957d5a69ce86f2f66f956bef1b9
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-22 21:05:15 +0100

    test(network): refactor KtorEitherSupportTest to use Kotest

--- cinescout/network/src/commonTest/kotlin/cinescout/network/KtorEitherSupportTest.kt
+++ cinescout/network/src/commonTest/kotlin/cinescout/network/KtorEitherSupportTest.kt
@@ -3,121 +3,88 @@ package cinescout.network
 import arrow.core.Either
 import arrow.core.left
 import cinescout.error.NetworkError
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.MockRequestHandleScope
 import io.ktor.client.engine.mock.respondError
-import io.ktor.client.plugins.SocketTimeoutException
+import io.ktor.client.network.sockets.SocketTimeoutException
+import io.ktor.client.request.HttpRequestData
+import io.ktor.client.request.HttpResponseData
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
-import kotlinx.coroutines.test.runTest
 import java.net.ConnectException
 import java.net.SocketException
 import java.net.UnknownHostException
 import javax.net.ssl.SSLHandshakeException
-import kotlin.test.Test
-import kotlin.test.assertEquals
 
-class KtorEitherSupportTest {
+class KtorEitherSupportTest : BehaviorSpec({
 
-    @Test
-    fun handles_ConnectException() = runTest {
-        // given
-        val expected = NetworkError.NoNetwork.left()
-        val client = CineScoutClient(
-            engine = MockEngine {
-                throw ConnectException("host")
+    Given("converting to Either") {
+
+        When("ConnectionException in thrown") {
+            val scenario = TestScenario { throw ConnectException("host") }
+
+            Then("NoNetwork is returned") {
+                scenario.execute() shouldBe NetworkError.NoNetwork.left()
             }
-        )
+        }
 
-        // when
-        val result: Either<NetworkError, String> = Either.Try { client.get("url").body() }
+        When("SocketException is thrown") {
+            val scenario = TestScenario { throw SocketException("Software caused connection abort") }
 
-        // then
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun handles_SocketException() = runTest {
-        // given
-        val expected = NetworkError.Unreachable.left()
-        val client = CineScoutClient(
-            engine = MockEngine {
-                throw SocketException("Software caused connection abort")
+            Then("Unreachable is returned") {
+                scenario.execute() shouldBe NetworkError.Unreachable.left()
             }
-        )
+        }
 
-        // when
-        val result: Either<NetworkError, String> = Either.Try { client.get("url").body() }
+        When("SocketTimeoutException is thrown") {
+            val scenario = TestScenario { throw SocketTimeoutException("Read timed out") }
 
-        // then
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun handles_SocketTimeoutException() = runTest {
-        // given
-        val expected = NetworkError.Unreachable.left()
-        val client = CineScoutClient(
-            engine = MockEngine { requestData ->
-                throw SocketTimeoutException(requestData)
+            Then("Unreachable is returned") {
+                scenario.execute() shouldBe NetworkError.Unreachable.left()
             }
-        )
+        }
 
-        // when
-        val result: Either<NetworkError, String> = Either.Try { client.get("url").body() }
+        When("SSLHandshakeException is thrown") {
+            val scenario = TestScenario { throw SSLHandshakeException("connection closed") }
 
-        // then
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun handles_SSLHandshakeException() = runTest {
-        // given
-        val expected = NetworkError.NoNetwork.left()
-        val client = CineScoutClient(
-            engine = MockEngine {
-                throw SSLHandshakeException("connection closed")
+            Then("NoNetwork is returned") {
+                scenario.execute() shouldBe NetworkError.NoNetwork.left()
             }
-        )
+        }
 
-        // when
-        val result: Either<NetworkError, String> = Either.Try { client.get("url").body() }
+        When("UnknownHostException is thrown") {
+            val scenario = TestScenario { throw UnknownHostException("host") }
 
-        // then
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun handles_UnknownHostException() = runTest {
-        // given
-        val expected = NetworkError.NoNetwork.left()
-        val client = CineScoutClient(
-            engine = MockEngine {
-                throw UnknownHostException("host")
+            Then("NoNetwork is returned") {
+                scenario.execute() shouldBe NetworkError.NoNetwork.left()
             }
-        )
+        }
 
-        // when
-        val result: Either<NetworkError, String> = Either.Try { client.get("url").body() }
-
-        // then
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun handles_service_unavailable() = runTest {
-        // given
-        val expected = NetworkError.Unreachable.left()
-        val client = CineScoutClient(
-            engine = MockEngine {
+        When("service unavailable error") {
+            val scenario = TestScenario { _ ->
                 respondError(HttpStatusCode.ServiceUnavailable)
             }
-        )
 
-        // when
-        val result: Either<NetworkError, String> = Either.Try { client.get("url").body() }
-
-        // then
-        assertEquals(expected, result)
+            Then("Unreachable is returned") {
+                scenario.execute() shouldBe NetworkError.Unreachable.left()
+            }
+        }
     }
+})
+
+private class KtorEitherSupportTestScenario(
+    private val client: HttpClient
+) {
+    
+    suspend fun execute(): Either<NetworkError, String> = Either.Try { client.get("url").body() }
 }
+
+private fun TestScenario(handler: suspend MockRequestHandleScope.(HttpRequestData) -> HttpResponseData) =
+    KtorEitherSupportTestScenario(
+        client = CineScoutClient(engine = MockEngine(handler))
+    )
+
