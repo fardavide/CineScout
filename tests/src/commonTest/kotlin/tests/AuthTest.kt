6304ea137ad012109f0e029470234ad7ec6814fb
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-24 12:42:58 +0100

    test(network): add integration test for token refresh

--- tests/src/commonTest/kotlin/tests/AuthTest.kt
+++ tests/src/commonTest/kotlin/tests/AuthTest.kt
@@ -6,22 +6,35 @@ import cinescout.auth.tmdb.data.remote.BaseTmdbAuthorizeTokenUrl
 import cinescout.auth.tmdb.data.sample.TmdbRequestTokenSample
 import cinescout.auth.tmdb.domain.usecase.LinkToTmdb
 import cinescout.auth.tmdb.domain.usecase.NotifyTmdbAppAuthorized
+import cinescout.auth.trakt.data.TraktAuthLocalDataSource
 import cinescout.auth.trakt.domain.sample.TraktAuthorizationCodeSample
 import cinescout.auth.trakt.domain.usecase.LinkToTrakt
 import cinescout.auth.trakt.domain.usecase.NotifyTraktAppAuthorized
+import cinescout.movies.data.remote.trakt.testutil.TraktMoviesWatchlistJson
+import cinescout.movies.domain.sample.MovieSample
+import cinescout.movies.domain.usecase.GetAllWatchlistMovies
+import cinescout.network.testutil.setHandler
+import cinescout.network.trakt.testutil.respondTraktJsonPage
+import cinescout.test.mock.MockEngines
 import cinescout.test.mock.junit5.MockAppExtension
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.types.shouldBeInstanceOf
+import io.ktor.client.engine.mock.respondError
+import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import org.koin.test.inject
+import store.builder.localPagedDataOf
+import util.AuthHelper
 
 class AuthTest : BehaviorSpec({
     val mockAppExtension = MockAppExtension {
         appScope(TestScope(UnconfinedTestDispatcher()))
     }
     extensions(mockAppExtension)
+    val authHelper = AuthHelper()
 
     Given("app started") {
         val linkToTmdb: LinkToTmdb by mockAppExtension.inject()
@@ -48,6 +61,38 @@ class AuthTest : BehaviorSpec({
                     awaitItem().getOrNull().shouldBeInstanceOf<LinkToTrakt.State.UserShouldAuthorizeApp>()
                     notifyTraktAppAuthorized(TraktAuthorizationCodeSample.AuthorizationCode)
                     awaitItem() shouldBe LinkToTrakt.State.Success.right()
+                }
+            }
+        }
+    }
+
+    Given("Trakt is linked") {
+        mockAppExtension { newInstall() }
+        authHelper.givenLinkedToTrakt()
+
+        val getAllWatchlistMovies: GetAllWatchlistMovies by mockAppExtension.inject()
+        val traktAuthLocalDataSource: TraktAuthLocalDataSource by mockAppExtension.inject()
+
+        val initialAccessToken = traktAuthLocalDataSource.findTokens()?.accessToken
+        initialAccessToken shouldNotBe null
+
+        When("get watchlist") {
+
+            And("token is expired (401)") {
+                var callsCount = 0
+                MockEngines.trakt.movie.setHandler {
+                    if (callsCount++ == 0) respondError(HttpStatusCode.Unauthorized)
+                    else respondTraktJsonPage(TraktMoviesWatchlistJson.OneMovie)
+                }
+
+                Then("watchlist is fetched") {
+                    getAllWatchlistMovies().test {
+                        awaitItem() shouldBe localPagedDataOf(MovieSample.Inception).right()
+                    }
+                }
+
+                Then("token is refreshed") {
+                    traktAuthLocalDataSource.findTokens()?.accessToken shouldNotBe initialAccessToken
                 }
             }
         }
