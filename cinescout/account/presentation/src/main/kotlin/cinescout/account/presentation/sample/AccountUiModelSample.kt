9309fcc83b9f3991d33da480cd4d48eaca7b7b6b
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-24 09:19:30 +0100

    test(account): update AccountUiModelSample to use Large image

--- cinescout/account/presentation/src/main/kotlin/cinescout/account/presentation/sample/AccountUiModelSample.kt
+++ cinescout/account/presentation/src/main/kotlin/cinescout/account/presentation/sample/AccountUiModelSample.kt
@@ -7,13 +7,13 @@ import cinescout.account.presentation.model.AccountUiModel
 object AccountUiModelSample {
 
     val Tmdb = AccountUiModel(
-        imageUrl = AccountSample.Tmdb.gravatar?.getUrl(Gravatar.Size.MEDIUM),
+        imageUrl = AccountSample.Tmdb.gravatar?.getUrl(Gravatar.Size.LARGE),
         source = AccountUiModel.Source.Tmdb,
         username = AccountSample.Tmdb.username.value
     )
 
     val Trakt = AccountUiModel(
-        imageUrl = AccountSample.Trakt.gravatar?.getUrl(Gravatar.Size.MEDIUM),
+        imageUrl = AccountSample.Trakt.gravatar?.getUrl(Gravatar.Size.LARGE),
         source = AccountUiModel.Source.Trakt,
         username = AccountSample.Trakt.username.value
     )
