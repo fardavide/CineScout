d28b54cbac0d48ac154721c6985cf54bf5223930
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-25 15:04:41 +0100

    fix(account): refresh current account when linked

--- cinescout/account/domain/src/commonMain/kotlin/cinescout/account/domain/usecase/GetCurrentAccount.kt
+++ cinescout/account/domain/src/commonMain/kotlin/cinescout/account/domain/usecase/GetCurrentAccount.kt
@@ -9,6 +9,7 @@ import cinescout.auth.domain.usecase.IsTmdbLinked
 import cinescout.auth.domain.usecase.IsTraktLinked
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import org.koin.core.annotation.Factory
 import store.Refresh
@@ -27,18 +28,23 @@ class RealGetCurrentAccount(
 ) : GetCurrentAccount {
 
     override operator fun invoke(refresh: Refresh): Flow<Either<GetAccountError, Account>> = combine(
-        getTmdbAccount(refresh = refresh),
-        getTraktAccount(refresh = refresh),
         isTmdbLinked(),
         isTraktLinked()
-    ) { tmdbAccountEither, traktAccountEither, isTmdbLinked, isTraktLinked ->
+    ) { (isTmdbLinked, isTraktLinked) ->
         check(isTmdbLinked.not() || isTraktLinked.not()) {
             "Both accounts are connected: this is not supported"
         }
-        when {
-            isTmdbLinked -> tmdbAccountEither
-            isTraktLinked -> traktAccountEither
-            else -> GetAccountError.NotConnected.left()
+        isTmdbLinked to isTraktLinked
+    }.flatMapLatest { (isTmdbLinked, isTraktLinked) ->
+        combine(
+            getTmdbAccount(refresh = refresh),
+            getTraktAccount(refresh = refresh)
+        ) { tmdbAccountEither, traktAccountEither ->
+            when {
+                isTmdbLinked -> tmdbAccountEither
+                isTraktLinked -> traktAccountEither
+                else -> GetAccountError.NotConnected.left()
+            }
         }
     }
 }
