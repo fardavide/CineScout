6d789cdd20d232705b213d526cfc15a9b75470b3
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-27 09:52:33 +0100

    chore(design): add names to Previews.kt

--- cinescout/design/src/main/kotlin/cinescout/design/Previews.kt
+++ cinescout/design/src/main/kotlin/cinescout/design/Previews.kt
@@ -6,21 +6,78 @@ import androidx.compose.ui.tooling.preview.Preview
 @Suppress("PreviewNaming")
 object AdaptivePreviews {
 
-    @Preview(device = Devices.PHONE)
-    @Preview(device = "spec:width=411dp,height=891dp,orientation=landscape")
-    @Preview(device = Devices.FOLDABLE)
-    @Preview(device = Devices.TABLET)
+    @Preview(
+        device = Devices.PHONE,
+        name = Name.Phone
+    )
+    @Preview(
+        device = Spec.PhoneLandScape,
+        name = Name.PhoneLandscape
+    )
+    @Preview(
+        device = Devices.FOLDABLE,
+        name = Name.Foldable
+    )
+    @Preview(
+        device = Devices.TABLET,
+        name = Name.Tablet
+    )
     annotation class Plain
 
-    @Preview(showBackground = true, device = Devices.PHONE)
-    @Preview(showBackground = true, device = "spec:width=411dp,height=891dp,orientation=landscape")
-    @Preview(showBackground = true, device = Devices.FOLDABLE)
-    @Preview(showBackground = true, device = Devices.TABLET)
+    @Preview(
+        device = Devices.PHONE,
+        name = Name.Phone,
+        showBackground = true
+    )
+    @Preview(
+        device = Spec.PhoneLandScape,
+        name = Name.PhoneLandscape,
+        showBackground = true
+    )
+    @Preview(
+        device = Devices.FOLDABLE,
+        name = Name.Foldable,
+        showBackground = true
+    )
+    @Preview(
+        device = Devices.TABLET,
+        name = Name.Tablet,
+        showBackground = true
+    )
     annotation class WithBackground
 
-    @Preview(showSystemUi = true, device = Devices.PHONE)
-    @Preview(showSystemUi = true, device = "spec:width=411dp,height=891dp,orientation=landscape")
-    @Preview(showSystemUi = true, device = Devices.FOLDABLE)
-    @Preview(showSystemUi = true, device = Devices.TABLET)
+    @Preview(
+        device = Devices.PHONE,
+        name = Name.Phone,
+        showSystemUi = true
+    )
+    @Preview(
+        device = Spec.PhoneLandScape,
+        name = Name.PhoneLandscape,
+        showSystemUi = true
+    )
+    @Preview(
+        device = Devices.FOLDABLE,
+        name = Name.Foldable,
+        showSystemUi = true
+    )
+    @Preview(
+        device = Devices.TABLET,
+        name = Name.Tablet,
+        showSystemUi = true
+    )
     annotation class WithSystemUi
+
+    private object Name {
+
+        const val Foldable = "Foldable"
+        const val Phone = "Phone"
+        const val PhoneLandscape = "Phone Landscape"
+        const val Tablet = "Tablet"
+    }
+
+    private object Spec {
+
+        const val PhoneLandScape = "spec:width=411dp,height=891dp,orientation=landscape"
+    }
 }
