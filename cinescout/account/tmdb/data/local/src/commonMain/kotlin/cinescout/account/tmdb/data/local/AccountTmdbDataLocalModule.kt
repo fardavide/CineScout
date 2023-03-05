be8f82fb0bb086f5b4051663e071dec0cb88c9a3
Davide Giuseppe Farella <fardavide@gmail.com>
2023-03-05 10:56:24 +0100

    chore(account): remove account:tmdb:data:local module

--- cinescout/account/tmdb/data/local/src/commonMain/kotlin/cinescout/account/tmdb/data/local/AccountTmdbDataLocalModule.kt
+++
@@ -1,8 +0,0 @@
-package cinescout.account.tmdb.data.local
-
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
-
-@Module
-@ComponentScan
-class AccountTmdbDataLocalModule
