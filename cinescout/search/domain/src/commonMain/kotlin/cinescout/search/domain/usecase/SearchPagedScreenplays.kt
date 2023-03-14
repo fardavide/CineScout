af5f13f3fcff585325b1097153c6a132ddcd883f
Davide Giuseppe Farella <fardavide@gmail.com>
2023-03-14 21:12:25 +0100

    chore(suggestions): refactor LocalSuggestionDataSource with Screenplay

--- cinescout/search/domain/src/commonMain/kotlin/cinescout/search/domain/usecase/SearchPagedScreenplays.kt
+++ cinescout/search/domain/src/commonMain/kotlin/cinescout/search/domain/usecase/SearchPagedScreenplays.kt
@@ -4,8 +4,17 @@ import app.cash.paging.PagingData
 import cinescout.screenplay.domain.model.Screenplay
 import cinescout.screenplay.domain.model.ScreenplayType
 import kotlinx.coroutines.flow.Flow
+import org.koin.core.annotation.Factory
 
 interface SearchPagedScreenplays {
 
     operator fun invoke(type: ScreenplayType, query: String): Flow<PagingData<Screenplay>>
+}
+
+@Factory
+internal class RealSearchPagedScreenplays : SearchPagedScreenplays {
+
+    override fun invoke(type: ScreenplayType, query: String): Flow<PagingData<Screenplay>> {
+        TODO("Not yet implemented")
+    }
 }
