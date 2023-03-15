f8e16c352edb7039d924406c2fef3c0d54e53475
Davide Giuseppe Farella <fardavide@gmail.com>
2023-03-15 17:46:25 +0100

    chore: implement RealTmdbScreenplayRemoteDataSource with Screenplay

--- cinescout/screenplay/data/remote/tmdb/src/commonMain/kotlin/cinescout/screenplay/data/remote/tmdb/datasource/RealTmdbScreenplayRemoteDataSource.kt
+++ cinescout/screenplay/data/remote/tmdb/src/commonMain/kotlin/cinescout/screenplay/data/remote/tmdb/datasource/RealTmdbScreenplayRemoteDataSource.kt
@@ -6,7 +6,9 @@ import cinescout.screenplay.data.remote.datasource.TmdbScreenplayRemoteDataSourc
 import cinescout.screenplay.data.remote.tmdb.mapper.TmdbScreenplayKeywordMapper
 import cinescout.screenplay.data.remote.tmdb.mapper.TmdbScreenplayMapper
 import cinescout.screenplay.data.remote.tmdb.service.TmdbScreenplayService
+import cinescout.screenplay.domain.model.Genre
 import cinescout.screenplay.domain.model.Screenplay
+import cinescout.screenplay.domain.model.ScreenplayGenres
 import cinescout.screenplay.domain.model.ScreenplayKeywords
 import cinescout.screenplay.domain.model.TmdbScreenplayId
 import org.koin.core.annotation.Factory
@@ -28,6 +30,15 @@ internal class RealTmdbScreenplayRemoteDataSource(
     override suspend fun getScreenplay(id: TmdbScreenplayId): Either<NetworkError, Screenplay> = when (id) {
         is TmdbScreenplayId.Movie -> screenplayService.getMovie(id).map(screenplayMapper::toMovie)
         is TmdbScreenplayId.TvShow -> screenplayService.getTvShow(id).map(screenplayMapper::toTvShow)
+    }
+
+    override suspend fun getScreenplayGenres(
+        screenplayId: TmdbScreenplayId
+    ): Either<NetworkError, ScreenplayGenres> = when (screenplayId) {
+        is TmdbScreenplayId.Movie -> screenplayService.getMovieGenres(screenplayId)
+            .map { list -> ScreenplayGenres(genres = list.map { Genre(it.id, it.name) }, screenplayId) }
+        is TmdbScreenplayId.TvShow -> screenplayService.getTvShowGenres(screenplayId)
+            .map { list -> ScreenplayGenres(genres = list.map { Genre(it.id, it.name) }, screenplayId) }
     }
 
     override suspend fun getSimilar(
--- cinescout/screenplay/domain/src/commonMain/kotlin/cinescout/screenplay/domain/model/ScreenplayGenres.kt
+++ cinescout/screenplay/domain/src/commonMain/kotlin/cinescout/screenplay/domain/model/ScreenplayGenres.kt
@@ -1,24 +1,22 @@
 package cinescout.screenplay.domain.model
 
-import arrow.core.NonEmptyList
-
 sealed interface ScreenplayGenres {
 
-    val genres: NonEmptyList<Genre>
+    val genres: List<Genre>
     val screenplayId: TmdbScreenplayId
 }
 
-fun ScreenplayGenres(genres: NonEmptyList<Genre>, screenplayId: TmdbScreenplayId) = when (screenplayId) {
+fun ScreenplayGenres(genres: List<Genre>, screenplayId: TmdbScreenplayId) = when (screenplayId) {
     is TmdbScreenplayId.Movie -> MovieGenres(genres, screenplayId)
     is TmdbScreenplayId.TvShow -> TvShowGenres(genres, screenplayId)
 }
 
 data class MovieGenres(
-    override val genres: NonEmptyList<Genre>,
+    override val genres: List<Genre>,
     override val screenplayId: TmdbScreenplayId.Movie
 ) : ScreenplayGenres
 
 data class TvShowGenres(
-    override val genres: NonEmptyList<Genre>,
+    override val genres: List<Genre>,
     override val screenplayId: TmdbScreenplayId.TvShow
 ) : ScreenplayGenres
