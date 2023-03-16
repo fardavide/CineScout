01c519f98b7802403f3e5d6629db61838acc60e6
Davide Giuseppe Farella <fardavide@gmail.com>
2023-03-16 18:44:56 +0100

    test: fix jvm tests

--- cinescout/di/kotlin/src/jvmTest/kotlin/cinescout/di/CineScoutModuleTest.kt
+++ cinescout/di/kotlin/src/jvmTest/kotlin/cinescout/di/CineScoutModuleTest.kt
@@ -2,6 +2,7 @@ package cinescout.di
 
 import cinescout.di.kotlin.AppVersionQualifier
 import cinescout.di.kotlin.CineScoutModule
+import cinescout.screenplay.domain.model.ScreenplayType
 import cinescout.suggestions.domain.usecase.StartUpdateSuggestions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.test.TestScope
@@ -22,7 +23,9 @@ class CineScoutModuleTest : KoinTest {
     @Test
     fun `verify common modules`() {
         try {
-            checkKoinModules(listOf(CineScoutModule, extraModule))
+            checkKoinModules(listOf(CineScoutModule, extraModule)) {
+                withInstance(ScreenplayType.All)
+            }
         } catch (e: InstanceCreationException) {
             throw e.getRootCause()
         }
