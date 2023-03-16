a366955cc69a72626259f3e23d972bbcf16608a3
Davide Giuseppe Farella <fardavide@gmail.com>
2023-03-16 16:00:45 +0100

    feat: cache current page

--- cinescout/rating/data/src/commonMain/kotlin/cinescout/rating/data/mediator/RatingsRemoteMediator.kt
+++ cinescout/rating/data/src/commonMain/kotlin/cinescout/rating/data/mediator/RatingsRemoteMediator.kt
@@ -3,6 +3,7 @@ package cinescout.rating.data.mediator
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import app.cash.paging.RemoteMediator
+import cinescout.error.NetworkError
 import cinescout.rating.domain.model.ScreenplayWithPersonalRating
 import cinescout.screenplay.domain.model.ScreenplayType
 import cinescout.store5.FetchException
@@ -13,21 +14,34 @@ internal class RatingsRemoteMediator(
     private val syncRatings: SyncRatings
 ) : RemoteMediator<Int, ScreenplayWithPersonalRating>() {
 
+    private var lastPage = 0
+
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, ScreenplayWithPersonalRating>
     ): MediatorResult {
-        val nextPage = when (loadType) {
-            LoadType.REFRESH -> 0
-            LoadType.PREPEND -> state.pages.last().prevKey
-                ?: return MediatorResult.Success(endOfPaginationReached = false)
-            LoadType.APPEND -> state.pages.last().nextKey
-                ?: return MediatorResult.Success(endOfPaginationReached = true)
+        val page = when (loadType) {
+            LoadType.REFRESH -> 1
+            // Prepend is not supported
+            LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+            LoadType.APPEND -> {
+                // TODO: Get last page + 1
+                ++lastPage
+            }
         }
 
-        return syncRatings(listType, nextPage).fold(
-            ifLeft = { networkError -> MediatorResult.Error(FetchException(networkError)) },
-            ifRight = { MediatorResult.Success(endOfPaginationReached = false) }
+        return syncRatings(listType, page).fold(
+            ifLeft = { networkError ->
+                when (networkError) {
+                    is NetworkError.NotFound -> MediatorResult.Success(endOfPaginationReached = true)
+                    else -> MediatorResult.Error(FetchException(networkError))
+                }
+            },
+            ifRight = {
+                // TODO: Store current page page
+                lastPage = page
+                MediatorResult.Success(endOfPaginationReached = false)
+            }
         )
     }
 }
--- cinescout/watchlist/data/src/commonMain/kotlin/cinescout/watchlist/data/mediator/WatchlistRemoteMediator.kt
+++ cinescout/watchlist/data/src/commonMain/kotlin/cinescout/watchlist/data/mediator/WatchlistRemoteMediator.kt
@@ -3,6 +3,7 @@ package cinescout.watchlist.data.mediator
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import app.cash.paging.RemoteMediator
+import cinescout.error.NetworkError
 import cinescout.screenplay.domain.model.Screenplay
 import cinescout.screenplay.domain.model.ScreenplayType
 import cinescout.store5.FetchException
@@ -13,18 +14,31 @@ internal class WatchlistRemoteMediator(
     private val syncWatchlist: SyncWatchlist
 ) : RemoteMediator<Int, Screenplay>() {
 
+    private var lastPage = 0
+
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Screenplay>): MediatorResult {
-        val nextPage = when (loadType) {
-            LoadType.REFRESH -> 0
-            LoadType.PREPEND -> state.pages.last().prevKey
-                ?: return MediatorResult.Success(endOfPaginationReached = false)
-            LoadType.APPEND -> state.pages.last().nextKey
-                ?: return MediatorResult.Success(endOfPaginationReached = true)
+        val page = when (loadType) {
+            LoadType.REFRESH -> 1
+            // Prepend is not supported
+            LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+            LoadType.APPEND -> {
+                // TODO: Get last page + 1
+                ++lastPage
+            }
         }
 
-        return syncWatchlist(listType, nextPage).fold(
-            ifLeft = { networkError -> MediatorResult.Error(FetchException(networkError)) },
-            ifRight = { MediatorResult.Success(endOfPaginationReached = false) }
+        return syncWatchlist(listType, page).fold(
+            ifLeft = { networkError ->
+                when (networkError) {
+                    is NetworkError.NotFound -> MediatorResult.Success(endOfPaginationReached = true)
+                    else -> MediatorResult.Error(FetchException(networkError))
+                }
+            },
+            ifRight = {
+                // TODO: Store current page page
+                lastPage = page
+                MediatorResult.Success(endOfPaginationReached = false)
+            }
         )
     }
 }
