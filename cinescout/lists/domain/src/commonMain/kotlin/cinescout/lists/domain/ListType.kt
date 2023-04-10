300f582b5c3125c96c012b592abf46341621a364
Davide Giuseppe Farella <fardavide@gmail.com>
2023-04-10 08:42:50 +0200

    chore(lists): remove ListType.kt

--- cinescout/lists/domain/src/commonMain/kotlin/cinescout/lists/domain/ListType.kt
+++
@@ -1,9 +0,0 @@
-package cinescout.lists.domain
-
-import cinescout.screenplay.domain.model.ScreenplayType
-
-@Deprecated(
-    "Use ScreenplayType instead",
-    ReplaceWith("ScreenplayType", "cinescout.screenplay.domain.model.ScreenplayType")
-)
-typealias ListType = ScreenplayType
