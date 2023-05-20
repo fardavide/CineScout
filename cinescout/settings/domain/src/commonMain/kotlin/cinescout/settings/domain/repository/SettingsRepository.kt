bd5313d64980aca3deae83c8cab6d65bb81ef376
Davide Giuseppe Farella <fardavide@gmail.com>
2023-05-20 09:13:18 +0200

    feat(settings): implement RealUpdateSavedListOptions

--- cinescout/settings/domain/src/commonMain/kotlin/cinescout/settings/domain/repository/SettingsRepository.kt
+++ cinescout/settings/domain/src/commonMain/kotlin/cinescout/settings/domain/repository/SettingsRepository.kt
@@ -1,6 +1,8 @@
 package cinescout.settings.domain.repository
 
+import arrow.core.some
 import cinescout.settings.domain.model.AppSettings
+import cinescout.settings.domain.model.SavedListOptions
 import cinescout.settings.domain.model.SuggestionSettings
 import kotlinx.coroutines.flow.StateFlow
 
@@ -9,6 +11,12 @@ interface SettingsRepository {
     fun getSuggestionSettings(): StateFlow<SuggestionSettings>
 
     suspend fun updateAppSettings(block: (AppSettings) -> AppSettings)
+
+    suspend fun updateSavedListOptions(savedListOptions: SavedListOptions) {
+        updateAppSettings { appSettings ->
+            appSettings.copy(savedListOptions = savedListOptions.some())
+        }
+    }
 
     suspend fun updateSuggestionSettings(block: (SuggestionSettings) -> SuggestionSettings) {
         updateAppSettings { appSettings ->
--- cinescout/settings/domain/src/commonMain/kotlin/cinescout/settings/domain/usecase/UpdateSavedListOptions.kt
+++ cinescout/settings/domain/src/commonMain/kotlin/cinescout/settings/domain/usecase/UpdateSavedListOptions.kt
@@ -1,10 +1,22 @@
 package cinescout.settings.domain.usecase
 
 import cinescout.settings.domain.model.SavedListOptions
+import cinescout.settings.domain.repository.SettingsRepository
+import org.koin.core.annotation.Factory
 
 interface UpdateSavedListOptions {
 
     suspend operator fun invoke(savedListOptions: SavedListOptions)
+}
+
+@Factory
+internal class RealUpdateSavedListOptions(
+    private val settingsRepository: SettingsRepository
+) : UpdateSavedListOptions {
+
+    override suspend operator fun invoke(savedListOptions: SavedListOptions) {
+        settingsRepository.updateSavedListOptions(savedListOptions)
+    }
 }
 
 class FakeUpdateSavedListOptions : UpdateSavedListOptions {
