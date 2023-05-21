c6cfee0789b8dd95ae651e37c675c6ca39d322e0
Davide Giuseppe Farella <37957092+fardavide@users.noreply.github.com>
2023-05-21 12:34:57 +0200

    Update ForYouScreen.kt

--- cinescout/suggestions/presentation/src/main/kotlin/cinescout/suggestions/presentation/ui/ForYouScreen.kt
+++ cinescout/suggestions/presentation/src/main/kotlin/cinescout/suggestions/presentation/ui/ForYouScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
@@ -23,6 +24,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import cinescout.design.TestTag
 import cinescout.design.theme.CineScoutTheme
 import cinescout.design.theme.Dimens
 import cinescout.design.ui.CenteredProgress
@@ -93,6 +95,7 @@ internal fun ForYouScreen(
 
     ConstraintLayout(
         modifier = modifier
+            .testTag(TestTag.ForYou)
             .padding(verticalSpacing)
             .fillMaxSize()
     ) {
