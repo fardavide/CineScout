8aeec29aa50f22d13cd62e4fc35347c5ae79c20f
Davide Giuseppe Farella <fardavide@gmail.com>
2023-08-19 18:24:50 +0200

    chore(utils): add ListUtils.kt docs

--- cinescout/utils/kotlin/src/commonMain/kotlin/cinescout/utils/kotlin/ListUtils.kt
+++ cinescout/utils/kotlin/src/commonMain/kotlin/cinescout/utils/kotlin/ListUtils.kt
@@ -2,32 +2,43 @@ package cinescout.utils.kotlin
 
 import arrow.core.Either
 import arrow.core.NonEmptyList
+import arrow.core.None
 import arrow.core.Option
 import arrow.core.right
 import arrow.core.toNonEmptyListOrNone
 import arrow.core.toNonEmptyListOrNull
 
-@Deprecated(
-    "Use toNonEmptyListOrNone) instead",
-    ReplaceWith("toNonEmptyListOrNone()", "arrow.core.toNonEmptyListOrNone")
-)
-fun <T> List<T>.nonEmpty(): Option<NonEmptyList<T>> = toNonEmptyListOrNone()
-
+/**
+ * @return [NonEmptyList] if the list is not empty, otherwise the result of [ifEmpty]
+ */
 fun <T, A> List<T>.nonEmpty(ifEmpty: () -> A): Either<A, NonEmptyList<T>> =
     toNonEmptyListOrNone().toEither(ifEmpty)
 
+/**
+ * @return [NonEmptyList] if the list is not empty, otherwise throws [IndexOutOfBoundsException]
+ * @throws IndexOutOfBoundsException if the list is empty
+ */
 fun <T> List<T>.nonEmptyUnsafe(): NonEmptyList<T> =
     toNonEmptyListOrNull() ?: throw IndexOutOfBoundsException("The list is empty.")
 
+/**
+ * @return random element of the list if not empty, otherwise [None]
+ */
 fun <T : Any> List<T>.randomOrNone(): Option<T> = Option.fromNullable(takeIf { isNotEmpty() }?.random())
 
+/**
+ * @return `true` if any element of the list is [Either.Right], otherwise `false`
+ */
 fun <A, B> List<Either<A, B>>.anyRight(): Boolean = any { either -> either.isRight() }
 
+/**
+ * @return list taking only [Either.Right] values
+ */
 fun <A, B> List<Either<A, B>>.filterRight(): List<B> =
     filterIsInstance<Either.Right<B>>().map { right -> right.value }
 
 /**
- * @return a list with only right values if any, otherwise the first left value.
+ * @return a list with only [Either.Right.value] if any, otherwise the first [Either.Left]
  */
 fun <A, B> List<Either<A, B>>.shiftWithAnyRight(): Either<A, List<B>> = when {
     isEmpty() -> emptyList<B>().right()
